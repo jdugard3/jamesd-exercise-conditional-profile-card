554dfdfe5cf95e15789efb8e4aa14cbd5894fddc
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-04-14 18:53:00 +0000

    added render functions to all except the positioning

--- src/js/app.js
+++ src/js/app.js
@@ -45,13 +45,47 @@ function render(variables = {}) {
                : variables.lastName
            }
           </h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h2>${
+            variables.role === null || variables.role === " "
+              ? (variables.role = "role")
+              : variables.role
+          }
+          </h2>
+          <h3>${
+            variables.city === null || variables.city === " "
+              ? (variables.city = "city")
+              : variables.city
+          }
+           
+           ${
+             variables.country === null || variables.country === " "
+               ? (variables.country = "country")
+               : variables.country
+           }
+          </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=${
+              variables.twitter === null || variables.twitter === " "
+                ? (variables.twitter = "https://twitter.com/4GeeksAcademy")
+                : variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github === null || variables.github === " "
+                ? (variables.github = "https://github.com/4geeksacademy")
+                : variables.github
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin === null || variables.linkedin === " "
+                ? (variables.linkedin =
+                    "https://www.linkedin.com/school/4geeksacademy/")
+                : variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram === null || variables.instagram === " "
+                ? (variables.instagram =
+                    "https://www.instagram.com/4geeksacademy/")
+                : variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
